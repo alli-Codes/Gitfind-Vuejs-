bb68b684132341ea3abab32796a5185466a19f7d
alli-Codes <allisonevans081@gmail.com>
2022-06-21 08:36:46 +0000

    Adding google Lato font-face

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,10 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+  
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
+	font-family: 'Lato', sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
