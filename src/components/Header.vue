4bd8c1f734a5afe6922dab3769491ad2e1290681
alli-Codes <allisonevans081@gmail.com>
2022-06-21 08:30:17 +0000

    Importing and using the Boxicons package and component

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header" class="bg-gray-100 h-16 px-4 flex justify-between items-center">
+    <Logo />
+    <div id="themeMode" class="flex">
+      <box-icon name='toggle-left' class="mx-2" />
+      <p>Light</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Logo from './Logo'
+  import 'boxicons'
+  export default {
+    name: "Header",
+    components: {
+      Logo
+    }
+  }
+</script>
