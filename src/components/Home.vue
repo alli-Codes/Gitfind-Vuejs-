3d3e32fd3e7f37d140612609eb9fdd1943c81030
alli-Codes <allisonevans081@gmail.com>
2022-06-21 10:27:14 +0000

    Styling the search-section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,36 @@
 <template>
-  <div id="homepage" class="bg-red-400 grow h-96">
-    
+  <div id="homepage" class="bg-red-400 flex flex-col grow h-96">
+    <section id="search__section" class="my-4 flex justify-center bg-green-300">
+      <div id="search__container" class="flex items-center flex-row-reverse grow w-60 bg-blue-100">
+        <box-icon name="search" />
+        <input class="h-8">
+      </div>
+    </section>
+
+    <section id="user__section">
+      <div id="user__description">
+        <div id="user__image"></div>
+        <div id="user__name">
+          <h4>Octocat</h4>
+          <p>The Octocat</p>
+        </div>
+      </div>
+      <div id="user__bio">
+        <p>Hello, I'm from Github.</p>
+      </div>
+      <div id="user__info">
+        
+      </div>
+
+      <div id="user__links">
+        
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+  import 'boxicons'
   export default {
     name: 'Home',
     
