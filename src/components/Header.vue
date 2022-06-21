ea1a3aa4ae8a85bc244bef60b3f4f9593af74086
alli-Codes <allisonevans081@gmail.com>
2022-06-21 09:16:19 +0000

    Adding a function that changes the toogler icons

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,8 @@
   <div id="header" class="bg-gray-100 h-16 px-4 flex justify-between items-center">
     <Logo />
     <div id="themeMode" class="flex items-center">
-      <box-icon name='toggle-left' class="mx-2" />
-      <p class="text-xs font-semibold">Light</p>
+      <box-icon :name="name" :type="type" class="mx-2" @click="switchToggler" />
+      <p class="text-xs font-semibold">{{modeName}}</p>
     </div>
   </div>
 </template>
@@ -13,8 +13,34 @@
   import 'boxicons'
   export default {
     name: "Header",
+    
     components: {
       Logo
+    },
+
+    data(){
+      return{
+        name: 'toggle-left',
+        type: 'regular',
+        isToggled: false,
+        modeName: 'Light'
+      }
+    },
+    
+    methods: {
+      switchToggler(){
+        if(!this.isToggled) {
+          this.name = 'toggle-right'
+          this.type = 'solid'
+          this.isToggled = true
+          this.modeName = 'Dark'
+        }else {
+          this.name = 'toggle-left'
+          this.type = 'regular'
+          this.isToggled = false
+          this.modeName = 'Light'
+        }
+      }
     }
   }
 </script>
