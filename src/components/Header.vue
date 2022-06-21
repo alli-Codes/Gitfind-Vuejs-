9f34b248b190e3b02c86a88735513bbb9fc2bd73
alli-Codes <allisonevans081@gmail.com>
2022-06-21 08:47:13 +0000

    Seting the font-weight of the p tag

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="header" class="bg-gray-100 h-16 px-4 flex justify-between items-center">
     <Logo />
-    <div id="themeMode" class="flex">
+    <div id="themeMode" class="flex items-center">
       <box-icon name='toggle-left' class="mx-2" />
-      <p>Light</p>
+      <p class="text-xs font-semibold">Light</p>
     </div>
   </div>
 </template>
