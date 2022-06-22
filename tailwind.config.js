1a6d77b31ff1da00695d48b371e1a6f1998f899d
alli-Codes <allisonevans081@gmail.com>
2022-06-22 08:05:45 +0000

    Customizing fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
 	darkMode: 'class',
 	theme: {
     extend: {
+      colors:{
+        'a-blue': '#00316B',
+        'a-green': '#00FFF0',
+      },
       fontFamily: {
         'sans': ['poppins']
       }
